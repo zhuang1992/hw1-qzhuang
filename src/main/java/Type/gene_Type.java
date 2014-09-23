2399f3174e8f89b8b3510a8d67863dcb6c947e80
zhuang1992 <zhuang1992@gmail.com>
2014-09-22 20:45:40 -0400

    a

--- src/main/java/Type/gene_Type.java
+++ src/main/java/Type/gene_Type.java
@@ -12,77 +12,75 @@ import org.apache.uima.cas.Feature;
 import org.apache.uima.jcas.tcas.Annotation_Type;
 
 public class gene_Type extends Annotation_Type {
- 
-  @Override
-  protected FSGenerator getFSGenerator() {return fsGenerator;}
 
-  private final FSGenerator fsGenerator = 
-    new FSGenerator() {
-      public FeatureStructure createFS(int addr, CASImpl cas) {
-         if (gene_Type.this.useExistingInstance) {
-           
-           FeatureStructure fs = gene_Type.this.jcas.getJfsFromCaddr(addr);
-           if (null == fs) {
-             fs = new gene(addr, gene_Type.this);
-           gene_Type.this.jcas.putJfsFromCaddr(addr, fs);
-           return fs;
-           }
-           return fs;
-        } else return new gene(addr, gene_Type.this);
-      }
-    };
+  @Override
+  protected FSGenerator getFSGenerator() {
+    return fsGenerator;
+  }
+
+  private final FSGenerator fsGenerator = new FSGenerator() {
+    public FeatureStructure createFS(int addr, CASImpl cas) {
+      if (gene_Type.this.useExistingInstance) {
+
+        FeatureStructure fs = gene_Type.this.jcas.getJfsFromCaddr(addr);
+        if (null == fs) {
+          fs = new gene(addr, gene_Type.this);
+          gene_Type.this.jcas.putJfsFromCaddr(addr, fs);
+          return fs;
+        }
+        return fs;
+      } else
+        return new gene(addr, gene_Type.this);
+    }
+  };
 
   public final static int typeIndexID = gene.typeIndexID;
 
   public final static boolean featOkTst = JCasRegistry.getFeatOkTst("Type.gene");
- 
+
   final Feature casFeat_id;
- 
-  final int     casFeatCode_id;
- 
+
+  final int casFeatCode_id;
+
   public String getId(int addr) {
-        if (featOkTst && casFeat_id == null)
+    if (featOkTst && casFeat_id == null)
       jcas.throwFeatMissing("id", "Type.gene");
     return ll_cas.ll_getStringValue(addr, casFeatCode_id);
   }
-  
+
   public void setId(int addr, String v) {
-        if (featOkTst && casFeat_id == null)
+    if (featOkTst && casFeat_id == null)
       jcas.throwFeatMissing("id", "Type.gene");
-    ll_cas.ll_setStringValue(addr, casFeatCode_id, v);}
-    
-  
+    ll_cas.ll_setStringValue(addr, casFeatCode_id, v);
+  }
+
   final Feature casFeat_geneName;
-  
-  final int     casFeatCode_geneName;
+
+  final int casFeatCode_geneName;
 
   public String getGeneName(int addr) {
-        if (featOkTst && casFeat_geneName == null)
+    if (featOkTst && casFeat_geneName == null)
       jcas.throwFeatMissing("geneName", "Type.gene");
     return ll_cas.ll_getStringValue(addr, casFeatCode_geneName);
   }
-  
+
   public void setGeneName(int addr, String v) {
-        if (featOkTst && casFeat_geneName == null)
+    if (featOkTst && casFeat_geneName == null)
       jcas.throwFeatMissing("geneName", "Type.gene");
-    ll_cas.ll_setStringValue(addr, casFeatCode_geneName, v);}
-    
-  
+    ll_cas.ll_setStringValue(addr, casFeatCode_geneName, v);
+  }
+
   public gene_Type(JCas jcas, Type casType) {
     super(jcas, casType);
-    casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
+    casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl) this.casType, getFSGenerator());
 
- 
     casFeat_id = jcas.getRequiredFeatureDE(casType, "id", "uima.cas.String", featOkTst);
-    casFeatCode_id  = (null == casFeat_id) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_id).getCode();
+    casFeatCode_id = (null == casFeat_id) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl) casFeat_id)
+            .getCode();
 
- 
     casFeat_geneName = jcas.getRequiredFeatureDE(casType, "geneName", "uima.cas.String", featOkTst);
-    casFeatCode_geneName  = (null == casFeat_geneName) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_geneName).getCode();
+    casFeatCode_geneName = (null == casFeat_geneName) ? JCas.INVALID_FEATURE_CODE
+            : ((FeatureImpl) casFeat_geneName).getCode();
 
   }
 }
-
-
-
-    
