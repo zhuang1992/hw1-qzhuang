166a64ecda75b1bb42226ea57bc64ebcf56e06f8
zhuang1992 <zhuang1992@gmail.com>
2014-09-22 20:45:11 -0400

    a

---
+++ src/main/java/Type/gene.java
@@ -0,0 +1,66 @@
+package Type;
+
+
+
+import org.apache.uima.jcas.JCas; 
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.jcas.cas.TOP_Type;
+
+import org.apache.uima.jcas.tcas.Annotation;
+
+
+public class gene extends Annotation {
+ 
+  public final static int typeIndexID = JCasRegistry.register(gene.class);
+
+  public final static int type = typeIndexID;
+
+  @Override
+  public              int getTypeIndexID() {return typeIndexID;}
+ 
+  protected gene() {}
+    
+ 
+  public gene(int addr, TOP_Type type) {
+    super(addr, type);
+    readObject();
+  }
+  
+  public gene(JCas jcas) {
+    super(jcas);
+    readObject();   
+  } 
+
+  public gene(JCas jcas, int begin, int end) {
+    super(jcas);
+    setBegin(begin);
+    setEnd(end);
+    readObject();
+  }   
+
+  private void readObject() {}
+     
+ 
+  public String getId() {
+    if (gene_Type.featOkTst && ((gene_Type)jcasType).casFeat_id == null)
+      jcasType.jcas.throwFeatMissing("id", "Type.gene");
+    return jcasType.ll_cas.ll_getStringValue(addr, ((gene_Type)jcasType).casFeatCode_id);}
+    
+  public void setId(String v) {
+    if (gene_Type.featOkTst && ((gene_Type)jcasType).casFeat_id == null)
+      jcasType.jcas.throwFeatMissing("id", "Type.gene");
+    jcasType.ll_cas.ll_setStringValue(addr, ((gene_Type)jcasType).casFeatCode_id, v);}    
+   
+    
+  public String getGeneName() {
+    if (gene_Type.featOkTst && ((gene_Type)jcasType).casFeat_geneName == null)
+      jcasType.jcas.throwFeatMissing("geneName", "Type.gene");
+    return jcasType.ll_cas.ll_getStringValue(addr, ((gene_Type)jcasType).casFeatCode_geneName);}
+    
+  public void setGeneName(String v) {
+    if (gene_Type.featOkTst && ((gene_Type)jcasType).casFeat_geneName == null)
+      jcasType.jcas.throwFeatMissing("geneName", "Type.gene");
+    jcasType.ll_cas.ll_setStringValue(addr, ((gene_Type)jcasType).casFeatCode_geneName, v);}    
+  }
+
+    
---
+++ src/main/java/Type/gene_Type.java
@@ -0,0 +1,88 @@
+package Type;
+
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.cas.impl.CASImpl;
+import org.apache.uima.cas.impl.FSGenerator;
+import org.apache.uima.cas.FeatureStructure;
+import org.apache.uima.cas.impl.TypeImpl;
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.impl.FeatureImpl;
+import org.apache.uima.cas.Feature;
+import org.apache.uima.jcas.tcas.Annotation_Type;
+
+public class gene_Type extends Annotation_Type {
+ 
+  @Override
+  protected FSGenerator getFSGenerator() {return fsGenerator;}
+
+  private final FSGenerator fsGenerator = 
+    new FSGenerator() {
+      public FeatureStructure createFS(int addr, CASImpl cas) {
+         if (gene_Type.this.useExistingInstance) {
+           
+           FeatureStructure fs = gene_Type.this.jcas.getJfsFromCaddr(addr);
+           if (null == fs) {
+             fs = new gene(addr, gene_Type.this);
+           gene_Type.this.jcas.putJfsFromCaddr(addr, fs);
+           return fs;
+           }
+           return fs;
+        } else return new gene(addr, gene_Type.this);
+      }
+    };
+
+  public final static int typeIndexID = gene.typeIndexID;
+
+  public final static boolean featOkTst = JCasRegistry.getFeatOkTst("Type.gene");
+ 
+  final Feature casFeat_id;
+ 
+  final int     casFeatCode_id;
+ 
+  public String getId(int addr) {
+        if (featOkTst && casFeat_id == null)
+      jcas.throwFeatMissing("id", "Type.gene");
+    return ll_cas.ll_getStringValue(addr, casFeatCode_id);
+  }
+  
+  public void setId(int addr, String v) {
+        if (featOkTst && casFeat_id == null)
+      jcas.throwFeatMissing("id", "Type.gene");
+    ll_cas.ll_setStringValue(addr, casFeatCode_id, v);}
+    
+  
+  final Feature casFeat_geneName;
+  
+  final int     casFeatCode_geneName;
+
+  public String getGeneName(int addr) {
+        if (featOkTst && casFeat_geneName == null)
+      jcas.throwFeatMissing("geneName", "Type.gene");
+    return ll_cas.ll_getStringValue(addr, casFeatCode_geneName);
+  }
+  
+  public void setGeneName(int addr, String v) {
+        if (featOkTst && casFeat_geneName == null)
+      jcas.throwFeatMissing("geneName", "Type.gene");
+    ll_cas.ll_setStringValue(addr, casFeatCode_geneName, v);}
+    
+  
+  public gene_Type(JCas jcas, Type casType) {
+    super(jcas, casType);
+    casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
+
+ 
+    casFeat_id = jcas.getRequiredFeatureDE(casType, "id", "uima.cas.String", featOkTst);
+    casFeatCode_id  = (null == casFeat_id) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_id).getCode();
+
+ 
+    casFeat_geneName = jcas.getRequiredFeatureDE(casType, "geneName", "uima.cas.String", featOkTst);
+    casFeatCode_geneName  = (null == casFeat_geneName) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_geneName).getCode();
+
+  }
+}
+
+
+
+    
